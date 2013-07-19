ed210cdc83ee85958427ccd520feeb69ae96c77f
Frank Fallon <frankfallon0@gmail.com>
2013-07-19 22:01:04 +0100

    initial commit, starting redevelopment of plugin from scratch

---
+++ src/main/java/com/teozcommunity/teozfrank/duelme/main/DuelMe.java
@@ -0,0 +1,11 @@
+package com.teozcommunity.teozfrank.duelme.main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Frank
+ * Date: 19/07/13
+ * Time: 21:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class DuelMe {
+}

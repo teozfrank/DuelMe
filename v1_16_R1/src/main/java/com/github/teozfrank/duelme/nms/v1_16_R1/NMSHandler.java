36b3229867e2d8a6406c9e13cf3c45e1a88a0e20
teozfrank <unconfigured@null.spigotmc.org>
2020-12-27 17:56:21 +0000

    Switch to spigot API for action bar and title.

--- v1_16_R1/src/main/java/com/github/teozfrank/duelme/nms/v1_16_R1/NMSHandler.java
+++ v1_16_R1/src/main/java/com/github/teozfrank/duelme/nms/v1_16_R1/NMSHandler.java
@@ -1,24 +1,16 @@
 package com.github.teozfrank.duelme.nms.v1_16_R1;
 
 import com.github.teozfrank.duelme.api.TitleActionbar;
-import net.minecraft.server.v1_16_R1.*;
-import org.bukkit.Bukkit;
-import org.bukkit.boss.BarColor;
-import org.bukkit.boss.BarStyle;
-import org.bukkit.boss.BossBar;
-import org.bukkit.craftbukkit.v1_16_R1.entity.CraftPlayer;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class NMSHandler implements TitleActionbar {
 
     @Override
     public void sendActionbar(Player player, String message) {
-        /*IChatBaseComponent icbc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + message + "\"}");
-        PacketPlayOutChat bar = new PacketPlayOutChat(icbc, ChatMessageType.GAME_INFO);
-        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(bar);*/
-        player.sendMessage(message);
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
     }
 
     @Override
@@ -29,14 +21,7 @@ public class NMSHandler implements TitleActionbar {
 
     @Override
     public void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
-        CraftPlayer craftplayer = (CraftPlayer) player;
-        PlayerConnection connection = craftplayer.getHandle().playerConnection;
-        IChatBaseComponent titleJSON = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + title + "\"}");
-        IChatBaseComponent subtitleJSON = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + subtitle + "\"}");
-        PacketPlayOutTitle titlePacket = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, titleJSON, fadeIn, stay, fadeOut);
-        PacketPlayOutTitle subtitlePacket = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.SUBTITLE, subtitleJSON);
-        connection.sendPacket(titlePacket);
-        connection.sendPacket(subtitlePacket);
+        player.sendTitle(title, subtitle, fadeIn, stay, fadeOut);
     }
 
     @Override
@@ -47,13 +32,13 @@ public class NMSHandler implements TitleActionbar {
 
     @Override
     public void sendBossbar(String title, String colorName, String styleName, Player player, Plugin plugin, int period) {
-        BarColor color = BarColor.valueOf(colorName.toUpperCase());
-        BarStyle style = BarStyle.valueOf(styleName.toUpperCase());
-        final BossBar bar = Bukkit.createBossBar(title, color, style);
-        bar.addPlayer(player);
-        bar.setVisible(true);
-        final double interval = 1.0 / (period * 20L);
-        new BukkitRunnable() {
+        //BarColor color = BarColor.valueOf(colorName.toUpperCase());
+        //BarStyle style = BarStyle.valueOf(styleName.toUpperCase());
+        //final BossBar bar = Bukkit.createBossBar(title, color, style);
+        //bar.addPlayer(player);
+        //bar.setVisible(true);
+        //final double interval = 1.0 / (period * 20L);
+        /*new BukkitRunnable() {
             @Override
             public void run() {
                 double progress = bar.getProgress();
@@ -66,7 +51,7 @@ public class NMSHandler implements TitleActionbar {
                     bar.setProgress(newProgress);
                 }
             }
-        }.runTaskTimerAsynchronously(plugin, 0, 1L);
+        }.runTaskTimerAsynchronously(plugin, 0, 1L);*/
     }
 }
 
